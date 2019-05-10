dbc5ece9f02a2576e3a2eb75bbaa52bad212b438
Kasun Kodagoda <kvkrusl@gmail.com>
2019-05-10 21:47:26 +0530

    Added DiscountBuild and Discount models.

---
+++ ImplementationFactoryExample/ImplementationFactoryExample.Web/Builders/DiscountBuilder.cs
@@ -0,0 +1,49 @@
+﻿using ImplementationFactoryExample.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationFactoryExample.Web.Builders
+{
+    public class DiscountBuilder
+    {
+        private Discount _discount = new Discount();
+
+        public DiscountBuilder WithMinimumItemCount(int count)
+        {
+            _discount.MinimumItemCount = count;
+            return this;
+        }
+
+        public DiscountBuilder WithMinimumBillAmount(double amount)
+        {
+            _discount.MinimumBillAmount = amount;
+            return this;
+        }
+
+        public DiscountBuilder WithPercentage(double percentage)
+        {
+            _discount.Percentage = percentage;
+            return this;
+        }
+
+        public DiscountBuilder WithDefaults()
+        {
+            _discount = new Discount
+            {
+                MinimumBillAmount = 1000,
+                MinimumItemCount = 3,
+                Percentage = 5
+            };
+            
+            return this;
+        }
+
+        public Discount Build()
+        {
+            return _discount;
+        }
+    }
+}
---
+++ ImplementationFactoryExample/ImplementationFactoryExample.Web/Models/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace ImplementationFactoryExample.Web.Models
+{
+    public class Discount
+    {
+        public int MinimumItemCount { get; set; }
+        public double MinimumBillAmount { get; set; }
+        public double Percentage { get; set; }
+    }
+}
