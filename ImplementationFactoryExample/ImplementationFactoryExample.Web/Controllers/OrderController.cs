992b81f0bd8ea6835127344c59986148a4883353
Kasun Kodagoda <kvkrusl@gmail.com>
2019-05-10 22:41:02 +0530

    Renamed and updated the controller

---
+++ ImplementationFactoryExample/ImplementationFactoryExample.Web/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ImplementationFactoryExample.Web.Models;
+using ImplementationFactoryExample.Web.Services.Contracts;
+
+namespace ImplementationFactoryExample.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IDiscountProcessor _discountProcessor;
+
+        public OrderController(IDiscountProcessor discountProcessor)
+        {
+            _discountProcessor = discountProcessor;
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new OrderViewModel();
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(OrderViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var discount = _discountProcessor.ProcessDiscount(model);
+                var checkoutViewModel = new CheckoutViewModel(model, discount);
+                return RedirectToAction("Checkout", checkoutViewModel);
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Checkout(CheckoutViewModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
